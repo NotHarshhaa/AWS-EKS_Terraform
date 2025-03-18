0d78791b71c300e1adc1f33dde787581366d7b55
NotHarshhaa <reddyharshhaa12@gmail.com>
2025-03-18 09:43:47 +0530

    aws-eks: Enabled DNS resolution, hostnames, and added environment tagging for better resource management.
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- vpc.tf
+++ vpc.tf
@@ -1,7 +1,10 @@
 resource "aws_vpc" "main" {
-  cidr_block = "10.0.0.0/16"
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
-    Name = "PC-VPC"
+    Name = "pc-vpc"
+    Environment = "dev"
   }
 }
