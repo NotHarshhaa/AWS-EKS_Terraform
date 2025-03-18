f77a42bc7768cdb27c6f45e974c2963ede520c40
NotHarshhaa <reddyharshhaa12@gmail.com>
2025-03-18 09:45:35 +0530

    aws-eks: Added private subnets, improved naming conventions, and added environment tagging.
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- subnets.tf
+++ subnets.tf
@@ -1,21 +1,45 @@
-resource "aws_subnet" "public-1" {
+resource "aws_subnet" "public_1" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"
   availability_zone       = "us-east-1a"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-sub-1"
+    Name        = "public-sub-1"
+    Environment = "dev"
   }
 }
 
-resource "aws_subnet" "public-2" {
+resource "aws_subnet" "public_2" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.2.0/24"
   availability_zone       = "us-east-1b"
   map_public_ip_on_launch = true
 
   tags = {
-    Name = "public-sub-2"
+    Name        = "public-sub-2"
+    Environment = "dev"
+  }
+}
+
+resource "aws_subnet" "private_1" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.3.0/24"
+  availability_zone = "us-east-1a"
+
+  tags = {
+    Name        = "private-sub-1"
+    Environment = "dev"
+  }
+}
+
+resource "aws_subnet" "private_2" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.4.0/24"
+  availability_zone = "us-east-1b"
+
+  tags = {
+    Name        = "private-sub-2"
+    Environment = "dev"
   }
 }
