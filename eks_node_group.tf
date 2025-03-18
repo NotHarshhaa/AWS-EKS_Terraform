ca6c556d1776335c18f07862e005e70610b1fbcf
NotHarshhaa <reddyharshhaa12@gmail.com>
2025-03-18 09:53:30 +0530

    aws-eks: Improved naming consistency, removed unnecessary `tomap()`, updated dependencies, and cleaned up comments.
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- eks_node_group.tf
+++ eks_node_group.tf
@@ -1,32 +1,33 @@
-resource "aws_instance" "kubectl-server" {
+resource "aws_instance" "kubectl_server" {
   ami                         = "ami-06ca3ca175f37dd66"
-  key_name                    = "EKSKEYPAIR"
+  key_name                    = "EKS_KEY_PAIR"
   instance_type               = "t2.micro"
   associate_public_ip_address = true
   subnet_id                   = aws_subnet.public-1.id
   vpc_security_group_ids      = [aws_security_group.allow_tls.id]
 
   tags = {
-    Name = "kubectl"
+    Name = "kubectl-server"
   }
-
 }
 
-resource "aws_eks_node_group" "node-grp" {
+resource "aws_eks_node_group" "eks_node_group" {
   cluster_name    = aws_eks_cluster.eks.name
   node_group_name = "pc-node-group"
-  node_role_arn   = aws_iam_role.worker.arn
+  node_role_arn   = aws_iam_role.eks_worker_role.arn
   subnet_ids      = [aws_subnet.public-1.id, aws_subnet.public-2.id]
   capacity_type   = "ON_DEMAND"
-  disk_size       = "20"
+  disk_size       = 20
   instance_types  = ["t2.small"]
 
   remote_access {
-    ec2_ssh_key               = "EKSKEYPAIR"
+    ec2_ssh_key               = "EKS_KEY_PAIR"
     source_security_group_ids = [aws_security_group.allow_tls.id]
   }
 
-  labels = tomap({ env = "dev" })
+  labels = {
+    env = "dev"
+  }
 
   scaling_config {
     desired_size = 2
@@ -39,10 +40,8 @@ resource "aws_eks_node_group" "node-grp" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.AmazonEC2ContainerRegistryReadOnly,
-    #aws_subnet.pub_sub1,
-    #aws_subnet.pub_sub2,
+    aws_iam_role_policy_attachment.eks_worker_node_policy,
+    aws_iam_role_policy_attachment.eks_cni_policy,
+    aws_iam_role_policy_attachment.ec2_container_registry_read_only
   ]
 }
