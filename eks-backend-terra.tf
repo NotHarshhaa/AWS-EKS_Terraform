a055ff606bc2ad49fb5559c47a6b7c5ccb4ce205
NotHarshhaa <reddyharshhaa12@gmail.com>
2025-03-18 09:42:41 +0530

    aws-eks: Enhanced security with state encryption and improved naming conventions.
    Signed-off-by: NotHarshhaa <reddyharshhaa12@gmail.com>

--- eks-backend-terra.tf
+++ eks-backend-terra.tf
@@ -1,8 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "BUCKET_NAME"
-    key    = "backend/FILE_NAME_TO_STORE_STATE.tfstate"
-    region = "us-east-1"
-    dynamodb_table = "dynamoDB_TABLE_NAME"
+    bucket         = "your-s3-bucket-name"
+    key            = "terraform/state/eks-cluster.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "your-dynamodb-table-name"
   }
 }
